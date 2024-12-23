09adf72adc3890c96536cf40c7fa2e19c1aeed2a
Peter Hadlaw <peterhadlaw>
2024-12-23 23:46:44 +0100

    disable eslint and typescript during build

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuild: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
